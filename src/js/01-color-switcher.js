dece8525e162d4c3855b6c2e4ce6633084db9818
Morelli01 <bkmz055@ukr.net>
2023-05-26 00:35:04 +0300

    ю

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.body;
+
+let intervalId = null;
+
+function startColorSwitcher() {
+  startButton.disabled = true;
+  intervalId = setInterval(() => {
+    const color = getRandomHexColor();
+    body.style.backgroundColor = color;
+  }, 1000);
+}
+
+function stopColorSwitcher() {
+  startButton.disabled = false;
+  clearInterval(intervalId);
+  body.removeAttribute('style');
+}
+
+startButton.addEventListener('click', startColorSwitcher);
+stopButton.addEventListener('click', stopColorSwitcher);
